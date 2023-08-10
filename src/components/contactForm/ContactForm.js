ed74571a0688f7b74d92b5d4b928deb8ea33dfc9
shaeeb <shaeebali@gmail.com>
2023-08-10 10:03:24 +0100

    complete contact form fields/props

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -21,6 +21,31 @@ export const ContactForm = ({
           placeholder="Contact Name"
         />
       </label>
+      <br />
+      <label>
+        <input 
+          type="tel"
+          name="phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          required
+          placeholder="Telephone Number"
+        />
+      </label>
+      <br />
+      <label>
+        <input 
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          placeholder="Email Address"
+          pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}" //mask for US numbers
+        />
+      </label>
+      <br />
+      <input type="submit" value="Add Contact" />
     </form>
   );
 };
