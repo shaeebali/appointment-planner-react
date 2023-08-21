c0d5ade83f1a49fb8abfc8fda9c5a7b60fe2871e
shaeeb <shaeebali@gmail.com>
2023-08-21 10:28:43 +0100

    update components and link them

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ContactPicker } from "../contactPicker/ContactPicker";
 
 const getTodayString = () => {
   const [month, day, year] = new Date()
@@ -21,6 +22,50 @@ export const AppointmentForm = ({
 }) => {
 
   return (
-    <></>
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+          placeholder="Appointment Title"
+        />
+      </label>
+      <br />
+      <label>
+        <ContactPicker
+          name="contact"
+          value={contact}
+          contacts={getContactNames()}
+          onChange={(e) => setContact(e.target.value)}
+          placeholder="Appointment With"
+        />
+      </label>
+      <br />
+      <label>
+        <input
+          type="date"
+          name="date"
+          min={getTodayString()}
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          required
+        />
+      </label>
+      <br />
+      <label>
+        <input
+          type="time"
+          name="time"
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+          required
+        />
+      </label>
+      <br />
+      <input type="submit" value="Add Appointment" />
+    </form>
   );
 };
--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -4,7 +4,9 @@ import Tile from "./tile/Tile"
 export const TileList = ({ tiles }) => {
   return (
     <div>
-      
+      {tiles.map((tile, index) => (
+        <Tile key={index} tile={tile} />
+      ))}
     </div>
   );
 };
